c35377b316921ac7da6fb722d68f41c66f8ab1e2
Luis Fernando Ttito Surco <luis.ttito@ucsp.edu.pe>
2020-02-14 17:23:28 -0500

    v1

---
+++ Assets/BicycleSimulator/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void CrearUnaPartida()
+    {
+        SceneManager.LoadScene("Environment");
+    }
+
+
+    public void UnirseUnaPartida()
+    {
+    }
+}
